860b8b12bb4ef0b5a1649113a19244b0b58e8402
Manikandan Lakshmanan <111625517+mani1916@users.noreply.github.com>
2023-07-13 16:48:23 +0530

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,7 +1,7 @@
 a=[4,2,56,3,1,4,56,2,2,7,3,2,3]
 
-for i in range (1,len(a)):
-    for j in range (0,len(a)-1):
+for i in range (len(a)-1):
+    for j in range (len(a)-i-1):
         if(a[j]<a[j+1]):
             temp=a[j]
             a[j]=a[j+1]
